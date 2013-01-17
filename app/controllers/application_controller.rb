6b07a16982bbf9613a53606feefdd1fc55a88abe
ycombinator <ycombinator@gmail.com>
2013-01-17 05:32:21 -0800

    Adding (the beginnings of) CORS support.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::API
 
+  after_filter :set_access_control_headers
+
   def root
     redirect_to "https://github.com/codeforamerica/cfa_api"
+  end
+
+
+  def set_access_control_headers(allowed_origins = '*')
+    if (request.headers['Origin']) 
+      response.headers['Access-Control-Allow-Origin'] = allowed_origins
+    end
   end
 
 end
