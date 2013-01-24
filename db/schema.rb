22754b7ff5829cc3a56ef4e823d8046dabfcd57d
Dave Guarino <daguar@gmail.com>
2013-01-23 16:04:46 -0800

    Add new schema back

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130123220000) do
+ActiveRecord::Schema.define(:version => 20130119225748) do
 
   create_table "fellows", :force => true do |t|
     t.string   "name"
